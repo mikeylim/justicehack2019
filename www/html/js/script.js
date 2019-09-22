a0130e43466f1fb71e5ecfe7df12bf8b129a22b0
Mike Dohyun Lim <mikey3437@gmail.com>
2019-09-22 12:48:48 -0700

    Add files via upload

--- www/html/js/script.js
+++ www/html/js/script.js
@@ -8,7 +8,7 @@ function checkAvailability() {
     // check if textfield is blank
     if(document.getElementById("company").value === ""){
         document.getElementById("errorMessage").style.color = "red"; // red
-        document.getElementById("errorMessage").innerHTML = "Please enter your desired Corporate name.";
+        document.getElementById("errorMessage").innerHTML = "Please enter your preferred corporation name.";
     } else{
         var compName = $('#company').val();
         // api call for checking the company name in BC
@@ -19,12 +19,12 @@ function checkAvailability() {
                 if(compName.toUpperCase() === dupName.toUpperCase()){
                     // found the exact match
                     document.getElementById("errorMessage").style.color = "red"; // red
-                    document.getElementById("errorMessage").innerHTML = "Corporate name '" + compName + "' already exists.";
+                    document.getElementById("errorMessage").innerHTML = "Corporation name '" + compName + "' already exists.";
                 } else{
                     if(res['results'].length >= 5){
                         // found similar matches with more than 5 results
                         document.getElementById("errorMessage").style.color = "red";
-                        document.getElementById("errorMessage").innerHTML = "'" + compName + "' doesn't exist, but we found 5 similar matches: " ;
+                        document.getElementById("errorMessage").innerHTML = "We found 5 similar matches: " ;
                         for(var i = 0; i <= 4; i++) {
                             var node = document.createElement("LI");                 // Create a <li> node
                             var textnode = document.createTextNode(res['results'][i]['names'][0]['text']);         // Create a text node
@@ -34,7 +34,7 @@ function checkAvailability() {
                     } else{
                         // found similar matches with less than 5 results
                         document.getElementById("errorMessage").style.color = "red";
-                        document.getElementById("errorMessage").innerHTML = "'" + compName + "' doesn't exist, but we found " + res['results'].length + " similar matches: ";
+                        document.getElementById("errorMessage").innerHTML = "We found " + res['results'].length + " similar matches: ";
                         for(var results in res['results']){              
                             var node = document.createElement("LI");                 // Create a <li> node
                             var textnode = document.createTextNode(res['results'][results]['names'][0]['text']);         // Create a text node
@@ -69,3 +69,37 @@ function enableCheck() {
     // $('#checkAvailability').prop("disabled", false);
     document.getElementById("checkAvailability").disabled = false;    
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function validate() {
+    if($('#numberedComp').checked || $('#companyName').checked) {
+        console.log("good")
+    } else{
+        document.getElementById("errors").innerHTML = "Please click" ;
+    }
+}
+
+// (function() {
+// 'use strict';
+// window.addEventListener('load', function() {
+// // Fetch all the forms we want to apply custom Bootstrap validation styles to
+// var forms = document.getElementsByClassName('needs-validation');
+// // Loop over them and prevent submission
+// var validation = Array.prototype.filter.call(forms, function(form) {
+// form.addEventListener('submit', function(event) {
+// if (form.checkValidity() === false) {
+// event.preventDefault();
+// event.stopPropagation();
+// }
+// form.classList.add('was-validated');
+// }, false);
+// });
+// }, false);
+// })();
